9d5085c1147390419933bcab8badfaa7f8c9b1df
WickedBrat <harshsrivastav123@gmail.com>
2017-12-07 02:38:30 +0530

    added buttons for mentoree

--- headerm.php
+++ headerm.php
@@ -28,7 +28,7 @@
     <!-- Navigation -->
     <nav class="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
       <div class="container">
-        <a class="navbar-brand" href="mentor.php">Mentor Registration</a>
+        <a class="navbar-brand" href="mentor.php">Mentor Portal</a>
         <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
           Menu
           <i class="fa fa-bars"></i>
@@ -54,8 +54,8 @@
         <div class="row">
           <div class="col-lg-8 col-md-10 mx-auto">
             <div class="page-heading">
-              <h1>Geekhaven Mentor</h1>
-              <span class="subheading">Fill the form below and select the number of mentorees.</span>
+              <h1>Mentor Portal</h1>
+              <span class="subheading">Select your mentor from the list below and press Submit.</span>
             </div>
           </div>
         </div>
