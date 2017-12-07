b170cdc1f820f90b4c9e82ab60f2c9a3a32909e2
WickedBrat <harshsrivastav123@gmail.com>
2017-12-07 19:48:10 +0530

    corrected db

--- index.php
+++ index.php
@@ -95,6 +95,9 @@ if (isset($authUrl)){
   //$result = $mysqli->query("SELECT COUNT(google_id) as usercount FROM google_users WHERE google_id=$user->id");
 	$user_count = $result->fetch_object()->usercount; //will return 0 if user doesn't exist
   
+  $mysqli->query("CREATE TABLE IF NOT EXISTS `google_users_mentors` (`google_id` decimal(21,0) NOT NULL, `google_name` varchar(60) NOT NULL, `google_email` varchar(60) NOT NULL, `google_link` varchar(60) NOT NULL, `google_picture_link` varchar(60) NOT NULL, `max_count` int(11) NOT NULL DEFAULT '3', PRIMARY KEY (`google_id`)) ENGINE=InnoDB DEFAULT CHARSET=latin1");
+
+
   $_SESSION['userid']=$user->id;
   
   //include("mentor.php");
