8bb6a80465d59e1bba9825784861d5c53391bc4e
WickedBrat <harshsrivastav123@gmail.com>
2017-12-08 14:58:16 +0530

    Final gram

--- headerl.php
+++ headerl.php
@@ -39,7 +39,7 @@
               <a class="nav-link" href="index.php">Home</a>
             </li>
             <li class="nav-item">
-            <a class='nav-link' href='/'>Log In</a>" ?>
+            <a class='nav-link' href='/'>Log In</a>
             </li>
             </li>
           </ul>
