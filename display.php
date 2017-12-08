0a3009d87e9149539f25171f8dc18ac7c15779da
WickedBrat <harshsrivastav123@gmail.com>
2017-12-08 17:07:34 +0530

    debugging_final

--- display.php
+++ display.php
@@ -8,15 +8,8 @@ $connect = mysqli_connect("us-cdbr-iron-east-05.cleardb.net", "b061db06849ed7", 
 $ret = mysqli_query($connect, "SELECT * FROM `google_users_mentors`");
 
 while ($data = mysqli_fetch_array($ret)) {
-
-
-var_dump($data);
-
-echo "<br><br><br>";
-    
-    echo "<label class='radio-inline'>";
-    echo "<input type='radio' name='optradio' value='".$data['google_email']."'>".$data['google_name'];
-    echo "</label>";
+ 
+    echo $data['google_name'].'<br>';
 }
 
 ?>
