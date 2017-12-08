43387627d8f0ca510662d8fcca2c67e749675acf
WickedBrat <harshsrivastav123@gmail.com>
2017-12-08 16:24:23 +0530

    debugging

--- display.php
+++ display.php
@@ -1,12 +1,17 @@
 <?php
 
 
-  $mysqli = mysqli_connect("us-cdbr-iron-east-05.cleardb.net", "b061db06849ed7", "e5239436", "heroku_fbd4d972ab0bf1a");
 
-  $d = $connect->query("SELECT * FROM `google_users_mentors`");
-  while($maxc = mysqli_fetch_array($d)){
-    echo $maxc['google_name']."<br>";
-  }
+
+$connect = mysqli_connect("us-cdbr-iron-east-05.cleardb.net", "b061db06849ed7", "e5239436", "heroku_fbd4d972ab0bf1a");
+
+$ret = mysqli_query($connect, "SELECT * FROM `google_users_mentors`");
+
+while ($data = mysqli_fetch_array($ret)) {
     
+    echo "<label class='radio-inline'>";
+    echo "<input type='radio' name='optradio' value='".$data['google_email']."'>".$data['google_name'];
+    echo "</label>";
+}
 
 ?>
