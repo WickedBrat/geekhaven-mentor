81189c37ad01964a8db0166eef2f33799e924cd1
WickedBrat <harshsrivastav123@gmail.com>
2017-12-09 19:03:22 +0530

    Deleted_10

--- phpmailer/PHPMailerAutoload.php
+++ phpmailer/PHPMailerAutoload.php
@@ -42,8 +42,11 @@ if (version_compare(PHP_VERSION, '5.1.2', '>=')) {
      * Fall back to traditional autoload for old PHP versions
      * @param string $classname The name of the class to load
      */
-    function __autoload($classname)
+    function spl_autoload_register($classname)
     {
         PHPMailerAutoload($classname);
     }
 }
+spl_autoload_register(function($class) {
+    include 'classes/' . $class . '.class.php';
+});
