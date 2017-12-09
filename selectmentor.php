7a4f47071250d89fbea0b6ac2632f0172a27e222
WickedBrat <harshsrivastav123@gmail.com>
2017-12-09 23:22:58 +0530

    Deleted_27

--- selectmentor.php
+++ selectmentor.php
@@ -39,7 +39,7 @@ if (isset($_POST['submit'])) {
             $mail->isSMTP();                                      // Set mailer to use SMTP
             $mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
             $mail->SMTPAuth = true;                               // Enable SMTP authentication
-            $mail->Username = '';                 // SMTP username
+            $mail->Username = $useremail;                 // SMTP username
             $mail->Password = 'fofknwkqhdquubrm';                           // SMTP password
             $mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
             $mail->Port = 465;                                    // TCP port to connect to
