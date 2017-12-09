f14af4327e29c41b8a91eeee41f459ae984be8c3
WickedBrat <harshsrivastav123@gmail.com>
2017-12-09 14:20:09 +0530

    adding email (test_11)

--- display.php
+++ display.php
@@ -11,65 +11,32 @@ while ($data = mysqli_fetch_array($ret)) {
  
     echo $data['google_name'].'&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;'.$data['max_count'].'<br>';
 }
-/*
-echo "Now starting to send mail<br><br>";
-$to="iec2016076@iiita.ac.in";
-$subject="Mentor Portal";
-$message="This is test";
-$headers = "From: harshsrivastav123@gmail.com \n";
-$headers .= "MIME-Version:1.0 \n";
-$headers .= "Content-type: text/html; charset=iso-8859-1 \n";
 
-if (mail($to, $subject, $message,$headers)) {
-    echo "Succesfully Sent!";
+require 'PHPMailerAutoload.php';
+
+$mail = new PHPMailer;
+
+$mail->isSMTP();                                      // Set mailer to use SMTP
+$mail->Host = 'smtp.mailgun.org';                     // Specify main and backup SMTP servers
+$mail->SMTPAuth = true;                               // Enable SMTP authentication
+$mail->Username = 'postmaster@mentor-portal.herokuapp.com';   // SMTP username
+$mail->Password = '18a547fe72108889db8bbd4548839ba5';                           // SMTP password
+$mail->SMTPSecure = 'tls';                            // Enable encryption, only 'tls' is accepted
+
+$mail->From = 'harshsrivastav123@gmail.com';
+$mail->FromName = 'Mailer';
+$mail->addAddress('iec2016076@iiita.ac.in');                 // Add a recipient
+
+$mail->WordWrap = 50;                                 // Set word wrap to 50 characters
+
+$mail->Subject = 'Hello';
+$mail->Body    = 'Testing some Mailgun awesomness';
+
+if(!$mail->send()) {
+    echo 'Message could not be sent.';
+    echo 'Mailer Error: ' . $mail->ErrorInfo;
 } else {
-    echo "Failed";
-}
-*/
-ini_set("SMTP", "smtp.mailgun.org");
-ini_set("sendmail_from", "harshsrivastav123@gmail.com");
-
-$message = "The mail message was sent with the following mail setting:\r\nSMTP = smtp.mailgun.org\r\nsmtp_port = 25\r\nsendmail_from = YourMail@address.com";
-
-$headers = "From: harshsrivastav123@gmail.com";
-
-
-if (mail("harshsrivastav123@gmail.com", "Testing", $message, $headers)) {
-    echo "Check your email now....<br/>";
-} else {
-    echo "Whoops....<br/>";
+    echo 'Message has been sent';
 }
 
-
-
-
-/*
-
-
-error_reporting(E_ALL ^ E_NOTICE ^ E_DEPRECATED ^ E_STRICT);
-
-set_include_path("." . PATH_SEPARATOR . ($UserDir = dirname($_SERVER['DOCUMENT_ROOT'])) . "/pear/php" . PATH_SEPARATOR . get_include_path());
-require_once ".heroku/php/lib/php/Mail.php";
-
-$host = "ssl://smtp.gmail.com";
-$username = "harshsrivastav@gmail.com";
-$password = "yesyouarethebest";
-$port = "465";
-$to = "iec2016076@iiita.ac.in";
-$email_from = "harshsrivastav@gmail.com";
-$email_subject = "Subject Line Here: " ;
-$email_body = "whatever you like" ;
-$email_address = "iec2016076@iiita.ac.in";
-
-$headers = array ('From' => $email_from, 'To' => $to, 'Subject' => $email_subject, 'Reply-To' => $email_address);
-$smtp = Mail::factory('smtp', array ('host' => $host, 'port' => $port, 'auth' => true, 'username' => $username, 'password' => $password));
-$mail = $smtp->send($to, $headers, $email_body);
-
-
-if (PEAR::isError($mail)) {
-echo("<p>" . $mail->getMessage() . "</p>");
-} else {
-echo("<p>Message successfully sent!</p>");
-}
-*/
 ?>
