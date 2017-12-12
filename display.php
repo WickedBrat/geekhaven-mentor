1002f8d6c204664db45baa1c8397df40c801d54f
WickedBrat <harshsrivastav123@gmail.com>
2017-12-12 23:43:04 +0530

    set to 0

--- display.php
+++ display.php
@@ -30,8 +30,11 @@ $ret = mysqli_query($connect, "SELECT * FROM `google_users_mentors`");
 </table>
 
  <?php
-/*
-$connect->query("UPDATE `google_users` SET selected=0 WHERE google_name='Snigdha Dobhal' ");
+
+$connect->query("UPDATE `google_users_mentors` SET max_count=0 WHERE google_name='Sashank Mishra' ");
+$connect->query("UPDATE `google_users_mentors` SET max_count=0 WHERE google_name='Dhvit Mehta' ");
+$connect->query("UPDATE `google_users_mentors` SET max_count=0 WHERE google_name='Abhinav Khare' ");
+$connect->query("UPDATE `google_users_mentors` SET max_count=0 WHERE google_name='Aditya Dewan' ");/*
 $connect->query("UPDATE `google_users_mentors` SET max_count=5 WHERE google_name='Siddhant Srivastav' ");
 $connect->query("ALTER TABLE `google_users` DROP COLUMN mentor_name");
 $connect->query("ALTER TABLE `google_users_mentors` ADD COLUMN full int(11) DEFAULT 0");
