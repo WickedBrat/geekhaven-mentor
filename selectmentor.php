78bb0c4dc028225064d71ef8a67527f786d81315
WickedBrat <harshsrivastav123@gmail.com>
2017-12-12 15:30:15 +0530

    Adding authentication

--- selectmentor.php
+++ selectmentor.php
@@ -8,81 +8,92 @@ $usenam = $_SESSION['username'];
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
-if (strpos($useremail,'2016') == true || strpos($useremail,'2015') == true) {
-    echo "<h1><center>No! You should not attempt to do this! I know you're a mentor. Go away!</center></h1>";
-} 
-else {
-        $maile=$_POST['optradio'];
-        $connect = mysqli_connect("us-cdbr-iron-east-05.cleardb.net", "b061db06849ed7", "e5239436", "heroku_fbd4d972ab0bf1a");
-
-        $ret = mysqli_query($connect, "SELECT * FROM `google_users_mentors` WHERE `max_count`>=0 ");
-        $retu = mysqli_query($connect, "SELECT * FROM `google_users`");
-
-        include("headerm.php");
-
-
-        require 'vendor/autoload.php';
-        $mail = new PHPMailer(true);                              // Passing `true` enables exceptions    
-
-        if (isset($_POST['submit'])) {
-            if(isset($_POST['optradio']))
-            {
-                
-                $d = $connect->query("SELECT max_count FROM `google_users_mentors` WHERE google_email='$maile'");
-                $maxc = mysqli_fetch_array($d);
-                $maxc['max_count'] = $maxc['max_count'] - 1;
-                $max = $maxc['max_count'];
-                try {
-                    //$mail->SMTPDebug = 2;                                 // Enable verbose debug output
-                    $mail->isSMTP();                                      // Set mailer to use SMTP
-                    $mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
-                    $mail->SMTPAuth = true;                               // Enable SMTP authentication
-                    $mail->Username = 'geekhaven@iiita.ac.in';                 // SMTP username
-                    //$mail->Password = 'fofknwkqhdquubrm'; 
-                    $mail->Password = 'g33kh@ven';                           // SMTP password
-                    $mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
-                    $mail->Port = 465;                                    // TCP port to connect to
-            
-                    //Recipients
-                    $mail->setFrom('geekhaven@iiita.ac.in', 'GeekHaven, IIITA');
-                    $mail->addAddress($maile, 'Mentor');     // Add a recipient
-            
-                    $mail->isHTML(true);                                  // Set email format to HTML
-                    $mail->Subject = 'Mentee Allotted';
-                    $mail->Body    = 'Greetings! Alloted mentee to you. His email is '.$useremail.' and his name is'.$usenam;
-            
-                    $mail->send();
-                // echo "mail sent";
-                } catch (Exception $e) {
-                    echo "There is some error!<br>";
-                }
-                echo "You have selected: ".$_POST['optradio'].". He has been sent a mail but Go ahead contact him!<br>";
-                $connect->query("UPDATE google_users_mentors SET max_count=$max WHERE google_email='$maile'");
-                $connect->query("UPDATE google_users SET selected=1 WHERE google_id=$userid");
-            }
-        }
-
-        $a = $connect->query("SELECT * FROM google_users WHERE google_id=$userid");
-        $c = mysqli_fetch_array($a);
-
-            if ($c['selected'] == 0) {
-                while ($data = mysqli_fetch_array($ret)) {
+if (isset($_SESSION['access_token'])) {
+    if (strpos($useremail,'2016') == true || strpos($useremail,'2015') == true) {
+        echo "<h1><center>No! You should not attempt to do this! I know you're a mentor. Go away!</center></h1>";
+    } 
+    else {
+            $maile=$_POST['optradio'];
+            $connect = mysqli_connect("us-cdbr-iron-east-05.cleardb.net", "b061db06849ed7", "e5239436", "heroku_fbd4d972ab0bf1a");
+    
+            $ret = mysqli_query($connect, "SELECT * FROM `google_users_mentors` WHERE `max_count`>=0 ");
+            $retu = mysqli_query($connect, "SELECT * FROM `google_users`");
+    
+            include("headerm.php");
+    
+    
+            require 'vendor/autoload.php';
+            $mail = new PHPMailer(true);                              // Passing `true` enables exceptions    
+    
+            if (isset($_POST['submit'])) {
+                if(isset($_POST['optradio']))
+                {
                     
-                    echo "<label class='radio-inline'>";
-                    echo "<input type='radio' name='optradio' value='".$data['google_email']."'>".$data['google_name'];
-                    echo "</label>";
+                    $d = $connect->query("SELECT max_count FROM `google_users_mentors` WHERE google_email='$maile'");
+                    $maxc = mysqli_fetch_array($d);
+                    $maxc['max_count'] = $maxc['max_count'] - 1;
+                    $max = $maxc['max_count'];
+                    try {
+                        //$mail->SMTPDebug = 2;                                 // Enable verbose debug output
+                        $mail->isSMTP();                                      // Set mailer to use SMTP
+                        $mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
+                        $mail->SMTPAuth = true;                               // Enable SMTP authentication
+                        $mail->Username = 'geekhaven@iiita.ac.in';                 // SMTP username
+                        //$mail->Password = 'fofknwkqhdquubrm'; 
+                        $mail->Password = 'g33kh@ven';                           // SMTP password
+                        $mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
+                        $mail->Port = 465;                                    // TCP port to connect to
+                
+                        //Recipients
+                        $mail->setFrom('geekhaven@iiita.ac.in', 'GeekHaven, IIITA');
+                        $mail->addAddress($maile, 'Mentor');     // Add a recipient
+                
+                        $mail->isHTML(true);                                  // Set email format to HTML
+                        $mail->Subject = 'Mentee Allotted';
+                        $mail->Body    = 'Greetings! Alloted mentee to you. His email is '.$useremail.' and his name is'.$usenam;
+                
+                        $mail->send();
+                    // echo "mail sent";
+                    } catch (Exception $e) {
+                        echo "There is some error!<br>";
+                    }
+                    echo "You have selected: ".$_POST['optradio'].". He has been sent a mail but Go ahead contact him!<br>";
+                    $connect->query("UPDATE google_users_mentors SET max_count=$max WHERE google_email='$maile'");
+                    $connect->query("UPDATE google_users SET selected=1 WHERE google_id=$userid");
+                }
+            }
+    
+            $a = $connect->query("SELECT * FROM google_users WHERE google_id=$userid");
+            $c = mysqli_fetch_array($a);
+    
+                if ($c['selected'] == 0) {
+                    while ($data = mysqli_fetch_array($ret)) {
+                        
+                        echo "<label class='radio-inline'>";
+                        echo "<input type='radio' name='optradio' value='".$data['google_email']."'>".$data['google_name'];
+                        echo "</label>";
+                    }
+                    
+                echo "<button type='submit' name='submit' class='btn btn-default'>Submit</button>";
+                    echo "</form>";
+                
+                } else {
+                    echo "You've selected your mentor! Contact him!";
                 }
                 
-            echo "<button type='submit' name='submit' class='btn btn-default'>Submit</button>";
-                echo "</form>";
-            
-            } else {
-                echo "You've selected your mentor! Contact him!";
-            }
-            
-        include("footerm.php");
-    }
-
-
+            include("footerm.php");
+        }
+    
+} else {
+    session_start();
+    $authUrl=$_SESSION['authURL'];
+    include("headerl.php");
+    echo '<div align="center">';
+    echo '<h3>Login with Gmail to continue</h3>';
+    echo '<div>Please click login button to connect to Google.</div>';
+    echo '<a class="login" href="' . $authUrl . '"><img src="https://developers.google.com/+/images/branding/sign-in-buttons/Red-signin_Google_base_44dp.png" /></a>';
+    echo '</div>';
+    include("footerm.php");
+}
 
 ?>
