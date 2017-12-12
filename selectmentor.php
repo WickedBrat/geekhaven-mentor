51a2c64e433833ec60a5374ccfc89232be1df679
WickedBrat <harshsrivastav123@gmail.com>
2017-12-12 12:08:33 +0530

    Added les than count

--- selectmentor.php
+++ selectmentor.php
@@ -8,7 +8,7 @@ $usenam = $_SESSION['username'];
 
 $connect = mysqli_connect("us-cdbr-iron-east-05.cleardb.net", "b061db06849ed7", "e5239436", "heroku_fbd4d972ab0bf1a");
 
-$ret = mysqli_query($connect, "SELECT * FROM `google_users_mentors`");
+$ret = mysqli_query($connect, "SELECT * FROM `google_users_mentors` WHERE `max_count`<=0 ");
 
 $retu = mysqli_query($connect, "SELECT * FROM `google_users`");
 
